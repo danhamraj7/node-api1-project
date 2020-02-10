3b8c3aa2e3a73e691192ad915570c4aa896f347b
danhamraj <danhamraj@gmail.com>
2020-02-10 15:53:11 -0500

    created post update and delete endpoints

--- index.js
+++ index.js
@@ -45,5 +45,75 @@ server.get("/api/users/:id", (req, res) => {
     });
 });
 
+//post
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Users.insert(req.body)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            "There was an error while saving the user to the database"
+        });
+      });
+  }
+});
+
+//update
+
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    Users.update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: "The user information could not be modified."
+        });
+      });
+  }
+});
+
+//destroy
+server.delete("/api/users/:id", (req, res) => {
+  Users.remove(req.params.id)
+
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: "the user was deleted."
+        });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+
+    .catch(() => {
+      res.status(500).json({ errorMessage: "The user could not be removed" });
+    });
+});
+
 const port = 4000;
 server.listen(port, () => console.log(`\n **API running on ${port}** \n`));
