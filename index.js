137e4ebfecf9b5700e76c7819c0f8645d1fcbfc2
danhamraj <danhamraj@gmail.com>
2020-02-10 15:31:43 -0500

    Installed pkgs and setup pkg.json, wrote get server and tested

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+//imp express
+const express = require("express");
+//imp database
+const Users = require("./data/db.js");
+
+const server = express();
+server.use(express.json()); //parse json to the body
+
+server.get("/", (req, res) => {
+  res.send({ API: "up and running...." });
+});
+
+const port = 4000;
+server.listen(port, () => console.log(`\n **API running on ${port}** \n`));
